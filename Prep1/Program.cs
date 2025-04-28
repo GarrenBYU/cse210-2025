33bbb3c71a07929f23ca5170fe5b17566fad91b8
GarrenBYU <mei21004@byui.edu>
2025-04-28 01:05:20 -0600

    initial program

---
+++ Prep1/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your first name? ");
+        String firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        String lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+    }
+}
